ae8f143009f00f099b6dc01dbc643e03ae3ffff9
Mike Vegeto <mike@bedrockdata.com>
2017-02-05 21:33:06 -0500

    move example into external project

--- example/processors/hello_world_spout.js
+++
@@ -1,4 +0,0 @@
-exports.handler = function(event, context, callback) {
-  context.emit('hello world', {text: 'hello world'})
-  callback();
-}
--- example/processors/reverse.js
+++
@@ -1,4 +0,0 @@
-exports.handler = function(event, context, callback) {
-  console.log("GOT STUFF IN REVERSE", event, context);
-  context.emit('reversed', event.text.split('').reverse().join(''));
-}
